22b28141a8ca175a14d7e5bc68a7a06aeda25eb5
Tien Nguyen <149658780+TienNguyenDinh@users.noreply.github.com>
2024-03-07 06:35:10 +0700

    feat(main): update server.js

--- api/server.js
+++ api/server.js
@@ -1,3 +1,7 @@
+const fs = require("fs")
+const path = require("path")
+const db = JSON.parse(fs.readFileSync(path.join("db.json")))
+
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
 
